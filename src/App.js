5a2afcd387719509fa33e033ee03a36bb457139c
Ken Bougoul <jobken080@gmail.com>
2026-05-03 12:24:45 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './Header';
+function App() {
+  return (
+    <div className="App">
+      <Header />
+        <main className="contenu">
+        <p>Bienvenue ! Cette application vous aide a trouver
+        votre ligne de bus a Dakar.</p>
+      </main>
+    </div>
+  );
+}
+export default App;
